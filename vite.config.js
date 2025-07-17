b5d80290facfcf2dc12043cf4f5dc101e8345aa3
Bhawya Misri <bmisri@tran.ad.com>
2025-07-17 19:52:34 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: '/', 
+  base: '/react-folder-inside-folder-deploy/react/react-clamp/newUIFlix-Clamp', 
 })
